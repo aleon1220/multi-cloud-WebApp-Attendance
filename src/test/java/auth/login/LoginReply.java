34d47bd19f6130537d755fee001cea0703447060
Andres LeonRangel <aleon1220@gmail.com>
2024-04-06 17:21:30 +1300

    UI make it work with no errors
    1. prep set up to make UI UX work rendering from server
    2. final goal is to have a docker-compose stack
    
    Signed-off-by: Andres LeonRangel <aleon1220@gmail.com>

--- src/test/java/auth/login/LoginReply.java
+++ src/test/java/auth/login/LoginReply.java
@@ -4,24 +4,12 @@ import com.google.gson.annotations.Expose;
 import com.google.gson.annotations.SerializedName;
 
 import entity.AuthenticationResult;
-import entity.ChallengeParameters;
 
 public class LoginReply {
 
-  @SerializedName("ChallengeParameters")
-  @Expose
-  private ChallengeParameters challengeParameters;
   @SerializedName("AuthenticationResult")
   @Expose
   private AuthenticationResult authenticationResult;
-
-  public ChallengeParameters getChallengeParameters() {
-    return challengeParameters;
-  }
-
-  public void setChallengeParameters(ChallengeParameters challengeParameters) {
-    this.challengeParameters = challengeParameters;
-  }
 
   public AuthenticationResult getAuthenticationResult() {
     return authenticationResult;
