642b076c83b3469efc4d3fb46a87f0bd3c408bbf
Andres LeonRangel <aleon1220@gmail.com>
2024-05-13 22:22:39 +1200

    enables jakarta security module
    - clean up of dependencies
    
    Signed-off-by: Andres LeonRangel <aleon1220@gmail.com>

--- build.gradle.kts
+++ build.gradle.kts
@@ -26,26 +26,22 @@ dependencies {
     // https://primefaces.github.io/primefaces
     // https://mvnrepository.com/artifact/org.primefaces/primefaces
     // implementation("org.primefaces:primefaces:primefaces-14.0.0-RC1")
-    implementation("org.primefaces:primefaces:14.0.0-RC2:jakarta")
+    implementation("org.primefaces:primefaces:14.0.0-RC3:jakarta")
     // https://mvnrepository.com/artifact/org.primefaces.extensions/primefaces-extensions
-    implementation("org.primefaces.extensions:primefaces-extensions:14.0.0-RC2:jakarta")
+    implementation("org.primefaces.extensions:primefaces-extensions:14.0.0-RC3:jakarta")
     // implementation(files("https://repo.maven.apache.org/maven2/org/primefaces/primefaces/14.0.0-RC1/primefaces-14.0.0-RC1-jakarta.jar"))
     // implementation(files("https://repo.maven.apache.org/maven2/org/primefaces/extensions/primefaces-extensions/14.0.0-RC1/primefaces-extensions-14.0.0-RC1-jakarta.jar"))
     // PrimeFaces Dependencies
     // https://primefaces.github.io/primefaces/14_0_0/#/gettingstarted/dependencies
-    // https://mvnrepository.com/artifact/org.apache.myfaces.core/myfaces-api Apache myFaces
-    // implementation("org.apache.myfaces.core:myfaces-api:4.0.2")  // API https://myfaces.apache.org
-    // implementation("org.apache.myfaces.core:myfaces-impl:4.0.2")
     // https://mvnrepository.com/artifact/org.glassfish/jakarta.faces
-    implementation("org.glassfish:jakarta.faces:4.0.5")
+    implementation("org.glassfish:jakarta.faces:4.0.6")
     // https://mvnrepository.com/artifact/org.apache.bval/bval-jsr
     // implementation("org.apache.poi:poi:5.2.5") // apache POI DataExporter (Excel XML)
     // implementation("com.rometools:rome:1.15.0") // FeedReader
-    // Apache Commons FileUpload 1.5
     // implementation("net.sf.barcode4j:barcode4j-light:2.3.0") // barcode4j-light
     // implementation("net.glxn.qrgen:qrgen:1.8.0") // qrgen QR Code support for Barcode
     // implementation("com.googlecode.owasp-java-html-sanitizer:owasp-java-html-sanitizer:20220608.1")
-    
+
     // Jakarta EE
     // https://mvnrepository.com/artifact/jakarta.enterprise/jakarta.enterprise.cdi-api
     implementation("jakarta.enterprise:jakarta.enterprise.cdi-api:4.0.1")
@@ -54,8 +50,14 @@ dependencies {
     // https://mvnrepository.com/artifact/jakarta.platform/jakartaee-api-parent
     implementation("jakarta.platform:jakartaee-api-parent:10.0.0")
     // https://mvnrepository.com/artifact/jakarta.ws.rs/jakarta.ws.rs-api
+    // https://mvnrepository.com/artifact/jakarta.security.enterprise/jakarta.security.enterprise-api
+    implementation("jakarta.security.enterprise:jakarta.security.enterprise-api:4.0.0-M2")
+    // https://mvnrepository.com/artifact/jakarta.validation/jakarta.validation-api
+    implementation("jakarta.validation:jakarta.validation-api:3.1.0-M2")
+
+    // Utilities
     // https://mvnrepository.com/artifact/org.glassfish.jersey.core/jersey-client
-    implementation("org.glassfish.jersey.core:jersey-client:3.1.5")
+    implementation("org.glassfish.jersey.core:jersey-client:4.0.0-M1")
     // implementation("org.glassfish.jersey.core:jersey-common:2.22.2")
     // https://github.com/google/gson library JSON serialization/deserialization
     implementation("com.google.code.gson:gson:2.10.1")
@@ -64,24 +66,25 @@ dependencies {
     // https://bitbucket.org/snakeyaml/snakeyaml/wiki/Documentation
     // https://mavenlibs.com/maven/dependency/org.yaml/snakeyaml
     implementation("org.yaml:snakeyaml:2.1")
-    
+    // https://mvnrepository.com/artifact/io.jsonwebtoken/jjwt-api
+    implementation("io.jsonwebtoken:jjwt-api:0.12.5")
+
     // QRFunctions
-    implementation("com.google.zxing:javase:3.5.1")
-    implementation("com.google.zxing:core:3.5.1")
-    
-    // TEST
-    // JUnit Jupiter API and Engine for unit testing
-    testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.2")
-    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.8.2")
-    testImplementation("org.junit.jupiter:junit-jupiter-api:5.7.0")
-    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.7.0")
-    // Mocking in tests
-    testImplementation("org.mockito:mockito-core:4.5.1")
-    // JWT Authentication
-    testImplementation("io.jsonwebtoken:jjwt:0.9.1")
+    implementation("com.google.zxing:javase:3.5.3")
+    implementation("com.google.zxing:core:3.5.3")
+    // JWT JSON Web Tokens
+    testImplementation("io.jsonwebtoken:jjwt-api:0.12.5")
     // https://central.sonatype.com/artifact/com.unboundid/unboundid-ldapsdk
-    implementation("com.unboundid:unboundid-ldapsdk:6.0.11")
+    implementation("com.unboundid:unboundid-ldapsdk:7.0.0")
     implementation("com.kstruct:gethostname4j:1.0.0")
+
+    // Unit testing
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.11.0-M1")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.10.2")
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.11.0-M1")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.10.2")
+    // Mocking tests
+    testImplementation("org.mockito:mockito-core:5.11.0")
 }
 
 fun setWarVersion() {
@@ -141,7 +144,6 @@ tasks.register<DefaultTask>("getAppVersion") {
     description = "Get current App version"
     getAppVersion()
 }
-
 
 tasks.war {
     archiveBaseName.set(warDeploymentName)
